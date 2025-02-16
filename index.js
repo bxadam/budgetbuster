358206633dfb372d203b9fbb133402eeb94807da
bxadam <bx.adam03@gmail.com>
2025-02-16 12:43:36 -0500

    dist folder test run, db successful

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDoc, getDocs } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { db, analytics } from "./utils/api";
 
-const firebaseApp = initializeApp();
-
-const db = getFirestore(firebaseApp);
-const auth = getAuth(firebaseApp);
-const userCollection = db.collection("users");
-
-function getUsers() {
-  db.collection("users").getDocs();
-  console.log(userCollection);
-}
-
-getUsers();
+console.log(db, "db found");
