315b9029811fdb72c8959e554d1f53e8274d38d4
gleonio <gleonio@gmail.com>
2025-02-16 23:51:33 -0800

    added auth component and expense category collection in firestore database

--- utils/api.jsx
+++ utils/api.jsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +23,7 @@ const app = initializeApp(firebaseConfig);
 
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 
 export { db, analytics };
